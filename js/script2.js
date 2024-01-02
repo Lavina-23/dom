1d5edb93d0cffc3242a6da87474e01cf1a9a58bf
Lavina-23 <lavinakohai3227@gmail.com>
2024-01-02 21:17:40 +0700

    feat: event bubbling

--- js/script2.js
+++ js/script2.js
@@ -15,6 +15,29 @@ btnClose.addEventListener("click", function () {
 // with DOM Traversal
 const close = document.querySelectorAll(".close"); // the result is an array, so need to use a loop
 
+// event bubbling, if we add an event to parent element, the child element will first execute the parent's event
+const cards = document.querySelectorAll(".card");
+cards.forEach(function (card) {
+  card.addEventListener("click", function (e) {
+    alert("ok");
+  });
+});
+
+// looping with foreach
+close.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.parentElement.style.display = "none";
+
+    // preventDefault() -> to stop the default event
+    // like tag a('close' tag is a) has a default event to navigate to another page
+    // so, if the target href is empty, it will simply refresh the page
+    e.preventDefault();
+
+    // to stop the event bubbling, we can use stopPropagation() method
+    e.stopPropagation();
+  });
+});
+
 // looping with for
 for (let i = 0; i < close.length; i++) {
   close[i].addEventListener("click", function (e) {
@@ -22,17 +45,6 @@ for (let i = 0; i < close.length; i++) {
     e.target.parentElement.style.display = "none";
   });
 }
-
-// looping with foreach
-close.forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    e.target.parentElement.style.display = "none";
-    // preventDefault() -> to stop the default event
-    // like tag a('close' tag is a) has a default event to navigate to another page
-    // so, if the target href is empty, it will simply refresh the page
-    e.preventDefault();
-  });
-});
 
 // to traverse the elements, we can use :
 const nama = document.querySelector(".nama");
@@ -48,3 +60,12 @@ console.log(nama.nextElementSibling); // return class telp, use this method to t
 // previousElementSibling -> element
 console.log(nama.previousElementSibling); // return class img
 console.log(nama.previousElementSibling.previousElementSibling); // retunr null
+
+// the most correct way
+const container = document.querySelector(".container");
+
+container.addEventListener("click", function (e) {
+  if (e.target.className == "close") {
+    e.target.parentElement.style.display = "none";
+  }
+});
